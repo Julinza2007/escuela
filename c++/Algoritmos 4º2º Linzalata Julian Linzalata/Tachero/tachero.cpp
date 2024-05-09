a55cca83a9fb17389eb660b9d380ffe2665c3b78
Julian <example@example.com>
2024-05-09 11:48:44 -0300

    Termino de hacer el ejercicio del tachero

--- c++/Algoritmos 4º2º Linzalata Julian Linzalata/Tachero/tachero.cpp
+++ c++/Algoritmos 4º2º Linzalata Julian Linzalata/Tachero/tachero.cpp
@@ -1,25 +1,21 @@
 #include <stdio.h>
 #define base 1000
-float kilometraje, total;
+float kilometraje, valor, total;
 main(){
 	
-	printf("Por favor indique distancia: ");
+	printf("Por favor indique distancia en kilometros: ");
 	scanf("%f", & kilometraje);
 	
-	if (kilometraje < 6)
+	if (kilometraje <= 6)
 	{
-	total= (kilometraje / 0.5) * 350 + base;		
-
-if (kilometraje >=6 && kilometraje < 12)
-	{
-	total= (kilometraje / 0.5) * 250 + base;		
-	}
-
+	total= (kilometraje / 0.5) * 350;		
 }
+else if (kilometraje <= 12)
+	{
+	total=(kilometraje / 0.5) * 250 + (6 / 0.5) * 350;		
+}
+total= total + base;
 
-	
-	
-
-	printf("La cantidad de kilometraje es %f \n", kilometraje);
-	printf("El total a pagar es: %f ", total);
+	printf("La cantidad de kilometraje que usted esta haciendo es de %f km \n", kilometraje);
+	printf("El total a pagar es de: $ %f", total);
 }
