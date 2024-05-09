c404ab4f061b674f95d5c169a3c98edcd6afcc05
Julian <example@example.com>
2024-05-09 11:54:36 -0300

    Modifico algunas cosas del avion para que quede mejor estructurado

--- c++/Algoritmos 4º2º Linzalata Julian Linzalata/Avion/Avion.cpp
+++ c++/Algoritmos 4º2º Linzalata Julian Linzalata/Avion/Avion.cpp
@@ -1,22 +1,20 @@
 #include <stdio.h>
-int cliente;
-float prec_boleto, num_cuo, monto_total, monto_cuo, descuento;
+int cliente, num_cuo;
+float prec_boleto, monto_total, monto_cuo, descuento;
 main(){
 	printf("Elija opciones de cliente: \n Elija opcion 1. Usted es cliente habitual. \n Elija opcion 2. Usted no es cliente habitual. \n");
 	scanf("%d", & cliente);
 	printf("Ingrese precio del boleto a pagar: ");
 	scanf("%f", & prec_boleto);
-	printf("Ingrese opcion de cuotas con sus respectivos intereses: \n Elija la cantidad de cuotas para hacer el pago (hasta 3 cuotas con intereses del 10 porciento), \n y (hasta 6 cuotas inclusive con intereses del 20 porciento) \n");
+	printf("Por favor elija la cantidad de cuotas para hacer el pago (hasta 3 cuotas con intereses del 10 porciento), \n y (hasta 6 cuotas inclusive con intereses del 20 porciento) \n");
 	scanf("%f", & num_cuo);
-	
-	
-	
+		
 	if (num_cuo <= 3)
 	{
 		monto_total= prec_boleto * 1.10;
 		monto_cuo= monto_total / num_cuo;
-		
 	}
+	
 	else if (num_cuo <= 6)
 	{
 		monto_total= prec_boleto * 1.2;
@@ -32,14 +30,11 @@ main(){
 		printf("El descuento por ser cliente habitual es %f \n", descuento);
 	}
 	
-	else {
+	else 
+	{
 		printf("Usted no tiene descuento \n");
 	}
+	
 	printf("El monto de las cuotas es de %f \n", monto_cuo);
 	
-	
-
-
-	
-	
-}
+	}
