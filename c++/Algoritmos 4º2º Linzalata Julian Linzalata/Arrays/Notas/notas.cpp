24f1971c9329981cf6367749e801d4466687ecbc
julinza <julinza@example.com>
2024-07-02 18:49:59 -0300

    Terminé el ejercicio de Algoritmos que nos dió para hacer en clase la profesora. Ahora voy a estudiar para la prueba de FOR, buena suerte Julián Martín Linzalata (yo). PD: Sigo enamorado de alguien...

--- c++/Algoritmos 4º2º Linzalata Julian Linzalata/Arrays/Notas/notas.cpp
+++ c++/Algoritmos 4º2º Linzalata Julian Linzalata/Arrays/Notas/notas.cpp
@@ -8,49 +8,64 @@ Informar:
 5. Promedio entre los desaprobados. 
 */
 #include <stdio.h>
-float notas[5], notaMax=0, cont_apro=0, porc_apro=0, acum=0, promedio=0, cont_mayor_prom=0, cont_menor_prom=0, porc_menor_prom=0;
-int i, N=5, estudiante_max=0;
-main(){
+
+float notas[10], notaMax=0, cont_apro=0, porc_apro=0, acum=0, promedio=0,
+cont_mayor_prom=0, cont_menor_prom=0, porc_menor_prom=0, cont_desapro=0,
+acum_desapro=0, prom_desapro=0;
+int i, N=10, estudiante_max=0;
+
+main(){	
 	for(i=0; i < N; i++){
 	printf("Ingrese del alumno %d la nota: ",  i + 1);
 	scanf("%f", &notas[i]);
 	
-	if(notas[i] >= 6){
-		cont_apro = cont_apro + 1;
-	}
+		if(notas[i] >= 6){
+			cont_apro = cont_apro + 1;
+		}
+		else if(notas[i] < 6){
+			cont_desapro = cont_desapro + 1;
+			acum_desapro = acum_desapro + notas[i];
+		}
 	
-	acum = acum + notas[i];
+		acum = acum + notas[i];
 	
-	if(notas[i] > notaMax){
+		if(notas[i] > notaMax){
             notaMax = notas[i];
-            estudiante_max = i + 1;
+           	estudiante_max = i + 1;
         }
 	}
+		
+	for(i=0; i < N; i++){
 	
-	/*SALIDAS*/
+		if(notas[i] > promedio){
+			cont_mayor_prom = cont_mayor_prom + 1;
+		}
+		else if(notas[i] < promedio){
+			cont_menor_prom = cont_menor_prom + 1;
+		}
+	}
+	
+	/*CALCULOS*/
+	
 	porc_apro = (cont_apro * 100) / N;
 	promedio = acum / N;
 	porc_menor_prom = (cont_menor_prom * 100) / N;
 	
-	
-	for(i=0; i < N; i++){
-		if(notas[i] > promedio){
-			cont_mayor_prom = cont_mayor_prom + 1;
-		}
-			else if(notas[i] < promedio){
-				cont_menor_prom = cont_menor_prom + 1;
-			}
-	
-		
+	if(cont_desapro > 0){
+		prom_desapro = acum_desapro / cont_desapro;
 	}
-	
+	else{
+		prom_desapro = 0;
+	}
+		
 	for (i=0; i < N; i++) {
-        printf("\nAlumno %d |%f| ", i + 1, notas[i]);
+    	printf("\nAlumno %d |%f| ", i + 1, notas[i]);
     }
     
     printf("\n\nEl porcentaje de estudiantes aprobados es de: %f", porc_apro);
-	printf("\nEl número de alumnos que obtuvieron notas mayores al promedio es: %f", cont_mayor_prom);
+	printf("\nEl numero de alumnos que obtuvieron notas mayores al promedio es: %f", cont_mayor_prom);
     printf("\nEl porcentaje de alumnos que obtuvieron notas menores al promedio es: %f", porc_menor_prom);
-    printf("\nLa calificación máxima es: %f, correspondiente al estudiante %d", notaMax, estudiante_max);
-	
+    printf("\nLa calificacion maxima es: %f, correspondiente al estudiante %d", notaMax, estudiante_max);
+	printf("\nEl promedio de notas entre los desaprobados es igual a: %f", prom_desapro);
+
 }
