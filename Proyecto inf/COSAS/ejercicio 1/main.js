e37657eef3a570db256f9a3a572930ffde8679b3
julinza <julinza@example.com>
2024-07-03 15:49:24 -0300

    HELLO BYE

--- Proyecto inf/COSAS/ejercicio 1/main.js
+++ Proyecto inf/COSAS/ejercicio 1/main.js
@@ -1,7 +1,8 @@
-document.addEventListener('click', handleClick);
+let keysDown = {};
+document.addEventListener('click' /*<--- Eso es una forma de entrada*/,  handleClick); // Para que el programa escuche el click vinculando el click con la funciíon handleClick
 function handleClick(event){
     if (event.target.textContent == 'Azul')
-        document.querySelector('body').style.background = 'blue';
+        document.querySelector('#btnAzul').style.border = '10px ridge blue';
     if (event.target.textContent == 'Amarillo')
-        document.querySelector('body').style.background = 'yellow';
+        document.querySelector('#btnAmarillo').style.border = '10px ridge yellow';
 }
