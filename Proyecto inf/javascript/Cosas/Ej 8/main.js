e21834221b4179fe5197328183a1db6ce35b1fae
Julinza <julinza@example.com>
2024-08-12 17:58:17 -0300

    Sigo haciendo ejercicio numero 8 de proyecto informatico pero no me salió, todo lo hice en clase el día 12/08/2024

---
+++ Proyecto inf/javascript/Cosas/Ej 8/main.js
@@ -0,0 +1,6 @@
+    let circulo= document.createElement('div');
+    circulo.style.borderRadius = `50%`;
+    circulo.style.width = '40px';
+    circulo.style.backgroundColor = `red`;
+    document.querySelector('body').append(circulo);
+
