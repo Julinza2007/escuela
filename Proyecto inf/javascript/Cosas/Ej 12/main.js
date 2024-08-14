c53fb4c1e31f3fb4b963f39cbfb622217645bbfd
Julian <example@example.com>
2024-08-14 00:30:50 -0300

    Termino ejercicio 12

--- Proyecto inf/javascript/Cosas/Ej 12/main.js
+++ Proyecto inf/javascript/Cosas/Ej 12/main.js
@@ -1,1 +1,29 @@
-        
+lista ={
+    Name: "Julinza",
+    Surname: "Linzalata",
+    Age: 20,
+    Sex: "Male"
+};
+
+function makeRow(obj) {
+    let table= document.createElement("table");
+    table.classList.add("table2");
+    let row2 = document.createElement("tr");
+    let row = document.createElement("tr");    
+    for (let prop in obj){
+        let campo = document.createElement("td");
+        campo.textContent = prop;
+        row.append(campo);
+        let cell = document.createElement("td");
+        cell.textContent = obj[prop];
+        row2.append(cell);
+    }
+    table.append(row);
+    table.append(row2);
+    document.body.append(table);
+}
+
+console.log(Object.keys(lista));
+
+makeRow(lista);
+
